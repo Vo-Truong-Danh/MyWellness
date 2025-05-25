321390a522d9ce8ecd305e7c29923c578bd3f09f
DanhDayNe <vtd1501@gmail.com>
2025-05-25 12:18:38 +0700

    fix login

--- lib/src/screens/account_setting/account_setting.dart
+++ lib/src/screens/account_setting/account_setting.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:my_wellness/src/screens/authentication/LoginPage.dart';
+
+import '../authentication/Auth.dart';
 
 class Account_Setting extends StatefulWidget {
   @override
@@ -9,11 +12,14 @@ class Account_Setting extends StatefulWidget {
 }
 
 class _Account_Setting extends State<Account_Setting> {
+    final user = FirebaseAuth.instance.currentUser;
 
-  final user=FirebaseAuth.instance.currentUser;
-
-  logOut() async{
+  logOut() async {
     await FirebaseAuth.instance.signOut();
+    Navigator.of(context).pushAndRemoveUntil(
+      MaterialPageRoute(builder: (context) => Auth()),
+          (Route<dynamic> route) => false,
+    );
   }
 
   @override
@@ -35,18 +41,22 @@ class _Account_Setting extends State<Account_Setting> {
             'Email: ${user?.email}',
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
           ),
-          SizedBox(height: 50,),
+          SizedBox(height: 50),
           ElevatedButton(
             style: TextButton.styleFrom(
               maximumSize: Size(200, 50),
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
               backgroundColor: Color(0xFF30C9B7),
-              foregroundColor: Colors.white
+              foregroundColor: Colors.white,
             ),
             onPressed: () {
               logOut();
             },
-            child: Center(child: Text('Đăng Xuất',style: TextStyle(fontSize: 17),)),
+            child: Center(
+              child: Text('Đăng Xuất', style: TextStyle(fontSize: 17)),
+            ),
           ),
         ],
       ),
